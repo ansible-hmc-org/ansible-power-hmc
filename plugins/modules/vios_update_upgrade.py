d6cf7ec62237169afa10786ee2e447619cc8197b
SreenidhiS1 <155409322+SreenidhiS1@users.noreply.github.com>
2024-11-06 09:34:21 +0530

    Update vios_update_upgrade.py

--- plugins/modules/vios_update_upgrade.py
+++ plugins/modules/vios_update_upgrade.py
@@ -410,7 +410,7 @@ def ensure_update_upgrade(module, params):
     try:
         hmc.updatevios(module.params['state'], configDict=attributes)
     except HmcError as error:
-        if  ‎HmcConstants.USER_AUTHORITY_ERR in repr(error):
+        if HmcConstants.USER_AUTHORITY_ERR in repr(error):
             logger.debug(repr(error))
             return False, None, None
         else:
