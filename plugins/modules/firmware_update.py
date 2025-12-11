73d31ffef59e913ecfba5a978f1b453235ff8089
Chiranthan M V <Chiranthan.M.V@ibm.com>
2025-12-11 23:07:59 +0530

    Documentation Chnages for firmware_update

--- plugins/modules/firmware_update.py
+++ plugins/modules/firmware_update.py
@@ -12,7 +12,7 @@ module: firmware_update
 short_description: Change firmware level on Managed Systems
 notes:
     - All operations support passwordless authentication.
-    - The module is idempotent. If tasked with updating or upgrading the HMC to a level equal to the current level,
+    - The module is idempotent. If tasked with updating or upgrading a managed system to a level equal to the current level,
       it will skip the operation and report the state as unchanged. If a lower level is requested (downgrade), the module
       will fail with an appropriate message.
 description:
@@ -91,7 +91,7 @@ options:
                ccc is the three character identifier of the specific level of Managed System LIC updates to retrieve.
                ppp is the three character identifier of the specific level of Power LIC updates to retrieve.
                This is only valid when the LIC type is both Managed System and Power.
-            -  Specify release1_level1,release2_level2,... to retrieve specific levels of LIC updates, even if disruptive.
+            -  Specify release1_level1,release2_level2,... to retrieve specific levels of LIC upgrades, even if disruptive.
                The level specified in each entry indicates the desired level
                for all components which are running the release specified in the entry.
             -  The level value must be enclosed in double quotes.
@@ -111,6 +111,7 @@ options:
 
 author:
     - Mario Maldonado (@Mariomds)
+    - Chiranthan M V (@chiranthanmv)
 '''
 
 EXAMPLES = r'''
@@ -130,10 +131,10 @@ EXAMPLES = r'''
       system_name: <System name/mtms>
       repository: sftp
       remote_repo:
-      hostname: <hostname/ip>
-      userid: <user>
-      passwd: <password>
-      directory: /repo/images/
+        hostname: <hostname/ip>
+        userid: <user>
+        passwd: <password>
+        directory: /repo/images/
       level: 01VL941_047
       state: upgraded
 '''
