bd7650d2bae2723f1ebbd9018fa27badb70199f7
ChiranthaN <53443074+chiranthanmv@users.noreply.github.com>
2025-12-16 19:01:58 +0530

    Update firmware_update.py

--- plugins/modules/firmware_update.py
+++ plugins/modules/firmware_update.py
@@ -69,13 +69,13 @@ options:
             passwd:
                 description:
                     - The password to use to log in to the remote FTP or SFTP server.
-                      The I(passwd) and I(sshkey) options are mutually exclusive in case if I(location_type=sftp).
+                      The I(passwd) and I(sshkey) options are mutually exclusive in case if I(repository=sftp).
                       This option is only valid when the firmware image is located on a remote FTP or SFTP server.
                 type: str
             sshkey_file:
                 description:
                     - The name of the file that contains the SSH private key.
-                      This option is only valid if I(location_type=sftp).
+                      This option is only valid if I(repository=sftp).
                 type: str
             directory:
                 description:
