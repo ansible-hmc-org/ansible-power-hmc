1b6783066cf051761f92112de05f3db2fd580e16
Navinakumar Kandakur <nkandak1@in.ibm.com>
2026-03-23 12:55:50 +0530

    Document correction

--- plugins/modules/vios_mapping_facts.py
+++ plugins/modules/vios_mapping_facts.py
@@ -16,7 +16,7 @@ DOCUMENTATION = '''
 module: vios_mapping_facts
 author:
     - Sreenidhi S(@SreenidhiS1)
-short_description: Returns the VIOS mapping of physical, logical, and virtual devices as facts
+short_description: Returns the VIOS mapping of physical, logical, and virtual devices as facts.
 notes:
     - This module requires the HMC login user to have specific permissions.
       To achieve this, the user should create a task role based on hmcsuperadmin with additional permissions,
@@ -50,7 +50,7 @@ notes:
       ViewDumps+ViewPowerManagement+ViewSPP)
     - Create a user with the above created task role.
 description:
-    - Returns the VIOS mapping of physical, logical, and virtual devices as facts
+    - Returns the VIOS mapping of physical, logical, and virtual devices as facts.
 requirements:
 - VIOS >= 2.2.5.0
 - Python >= 3.9
@@ -78,7 +78,7 @@ options:
                 type: str
     system_name:
         description:
-            - The name of the managed system
+            - The name or mtms (machine type model serial) of the managed system
         type: str
     vios_name:
         description:
@@ -152,22 +152,22 @@ options:
 
 EXAMPLES = '''
 - name: Populate the mapping facts with the mapping information for VSCSI
-  mapping_facts:
-    hmc_host: <host>
+  vios_mapping_facts:
+    hmc_host: <host_IP>
     hmc_auth:
-      username: <hscroot>
-      password: <hmcpass>
+      username: <hmc_user>
+      password: <hmc_pass>
     system_name: <system_name>
     vios_name: <vios_name>
     component: vscsi
     state: facts
 
 - name: Populate the mapping facts with the mapping information for NPIV device vfchost0
-  mapping_facts:
-    hmc_host: <host>
+  vios_mapping_facts:
+    hmc_host: <host_IP>
     hmc_auth:
-      username: <hscroot>
-      password: <hmcpass>
+      username: <hmc_user>
+      password: <hmc_pass>
     system_name: <system_name>
     vios_name: <vios_name>
     vadapter: vfchost0
@@ -175,22 +175,22 @@ EXAMPLES = '''
     state: facts
 
 - name: Populate the mapping facts with the mapping information for all devices
-  mapping_facts:
-    hmc_host: <host>
+  vios_mapping_facts:
+    hmc_host: <host_IP>
     hmc_auth:
-      username: <hscroot>
-      password: <hmcpass>
+      username: <hmc_user>
+      password: <hmc_pass>
     system_name: <system_name>
     vios_name: <vios_name>
     component: all
     state: facts
 
 - name: Populate the mapping facts with the mapping information for optical backing devices
-  mapping_facts:
-    hmc_host: <host>
+  vios_mapping_facts:
+    hmc_host: <host_IP>
     hmc_auth:
-      username: <hscroot>
-      password: <hmcpass>
+      username: <hmc_user>
+      password: <hmc_pass>
     system_name: <system_name>
     vios_name: <vios_name>
     types: optical
