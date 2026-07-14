efaedab9d3db5c9b09ec22b9967cce86c3488539
SreenidhiS1 <155409322+SreenidhiS1@users.noreply.github.com>
2026-07-14 22:32:11 +0530

    Update hmc_rest_client.py

--- plugins/module_utils/hmc_rest_client.py
+++ plugins/module_utils/hmc_rest_client.py
@@ -3028,8 +3028,7 @@ class HmcRestClient:
         return "Error: Profile creation failed with unknown error"
 
     def updatePartitionProfile(self, lpar_uuid, partition_uuid, patched_xml, force=False):
-        url = "https://{0}/rest/api/uom/LogicalPartition/{1}/LogicalPartitionProfile/{2}".format(
-            self.hmc_ip, lpar_uuid, partition_uuid)
+        url = "https://{0}/rest/api/uom/LogicalPartition/{1}/LogicalPartitionProfile/{2}".format(self.hmc_ip, lpar_uuid, partition_uuid)
         if force:
             url += "?force=true"
         header = {'X-API-Session': self.session,
@@ -3057,7 +3056,7 @@ class HmcRestClient:
         profile_name_elements = post_response.xpath("//ProfileName")
         if profile_name_elements:
             return 200, profile_name_elements[0].text
-        return "Error: Profile update failed with unknown error"
+        return "Error: Profile Updation failed with unknown error"
 
     def getVirtualSwitches(self, system_uuid):
         url = "https://{0}/rest/api/uom/ManagedSystem/{1}/VirtualSwitch".format(self.hmc_ip, system_uuid)
